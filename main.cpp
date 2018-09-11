ac780567d3573d1fb5ac5281e196dcb98af436b8
Jacob Buysse <jacob.buysse@gmail.com>
2018-09-10 20:14:46 -0500

    Adding real timestamp.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
+#include <ctime>
+#include <locale>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <string>
 using namespace std;
 
 void TimestampEcho(const string& value)
 {
+	auto t = time(nullptr);
+	auto tm = *localtime(&t);
 	istringstream in{ value };
 	string line;
 	while (getline(in, line))
-		cout << "[now] " << line << '\n';
+		cout << put_time("[%Y-%m-%d %H:%M:%S] ", &tm) << line << '\n';
 }
 
 int main(int argc, char** argv)
